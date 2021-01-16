b452e98f62ffab8d3e2619f380981baf177f9330
ARQMENDOZAP\mmp86 <azulnegro2321@gmail.com>
2021-01-16 09:56:28 -0500

    no message

---
+++ FDRMKL02_acelerometro/sdk_hal/sdk_hal_gpi.h
@@ -0,0 +1,13 @@
+/*
+ * sdk_hal_gpi.h
+ *
+ *  Created on: 16 ene. 2021
+ *      Author: mmp86
+ */
+
+#ifndef SDK_HAL_GPI_H_
+#define SDK_HAL_GPI_H_
+
+
+
+#endif /* SDK_HAL_GPI_H_ */
---
+++ FDRMKL02_acelerometro/sdk_hal/sdk_hal_gpio.c
@@ -0,0 +1,8 @@
+/*
+ * sdk_hal_gpio.c
+ *
+ *  Created on: 16 ene. 2021
+ *      Author: mmp86
+ */
+
+
